ec90390cbd91f0d8f78d991e202327a0d624cb0c
SridharAN12 <kongusridhar7@gmail.com>
2016-09-09 17:15:23 +0530

    Add files via upload

---
+++ evenInteger.java
@@ -0,0 +1,14 @@
+class evenInteger
+{
+public static void main(String args[])
+{
+int l=20;
+System.out.println("odd number between 1 and "+l);
+for(int i=1;i<=l;i++)
+{
+if(i%2==0){
+System.out.println(i +" ");
+}
+}
+}
+}
