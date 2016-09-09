2f13c893fc8ed7bd070fa306c01912edbb3b98ff
SridharAN12 <kongusridhar7@gmail.com>
2016-09-09 17:06:56 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,14 @@
+class odd
+{
+public static void main(String args[])
+{
+int l=30;
+System.out.println("odd number between 1 and "+l);
+for(int i=1;i<=l;i++)
+{
+if(i%2!=0){
+System.out.println(i +" ");
+}
+}
+}
+}
